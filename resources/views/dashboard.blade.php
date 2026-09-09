4210b6809be444d39ea4bf9ddda9572e58022b1f
Gunz9v <gunz9v@hotmail.com>
2026-09-09 16:14:51 -0500

    Fit rental orders table within viewport

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,7 @@
         }
     </script>
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="{{ asset('css/sos-redesign.css') }}?v=1.1.5">
+    <link rel="stylesheet" href="{{ asset('css/sos-redesign.css') }}?v=1.1.6">
     @livewireStyles
 </head>
 @php
